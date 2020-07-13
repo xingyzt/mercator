01a092422193fe32deaaa3623496e410097a4df5
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2020-07-13 12:54:04 -0700

    template

---
+++ mercator-filters.greasyfork.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Google Meet Filters & Transforms
+// @namespace    http://tampermonkey.net/
+// @version      1.5
+// @description  Change how you look on Google Meet.
+// @author       Xing
+// @match        https://meet.google.com/*
+// @grant        none
+// ==/UserScript==
+
+// MERCATOR FILTERS is made by Xing in 2020 under the MIT License
+
+{{mercator-filters.js}}
