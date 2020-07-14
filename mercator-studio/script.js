56ec3f5ca149b084c270ed77bd4903c055266f7c
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2020-07-14 11:35:33 -0700

    format CSS

--- mercator-studio/script.js
+++ mercator-studio/script.js
@@ -17,172 +17,168 @@ async function() {
 
 	style.textContent = `
 * {
-box-sizing: border-box;
+	box-sizing: border-box;
 }
 
-:focus{
+:focus {
 	outline: 0;
 }
 
 main {
-z-index: 99999;
-
-position: fixed;
-left: 0;
-top: 0;
-
-width: 480px;
-max-width: 100vw;
-height: auto;
-min-height: 100vh;
-overflow: scroll;
-
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-
-background: white;
-padding: 1rem;
-transform: translateY(calc(-100% + 4rem));
-box-shadow: 0 0 2rem #0002;
-border-radius:0 0 1rem 0;
-border: 1px solid #aaa;
-border-top: 0;
-border-left: 0;
-transition: transform 200ms;
+	z-index: 99999;
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 480px;
+	max-width: 100vw;
+	height: auto;
+	min-height: 100vh;
+	overflow: scroll;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	background: white;
+	padding: 1rem;
+	transform: translateY(calc(-100% + 4rem));
+	box-shadow: 0 0 2rem #0002;
+	border-radius: 0 0 1rem 0;
+	border: 1px solid #aaa;
+	border-top: 0;
+	border-left: 0;
+	transition: transform 200ms;
 }
 
-main:hover{
-transform: none;
-border-radius: 0;
-height: 100vh;
+main:hover {
+	transform: none;
+	border-radius: 0;
+	height: 100vh;
 }
 
-#previews{
-margin-top: 1rem;
+#previews {
+	margin-top: 1rem;
 }
 
 #previews>* {
-height: 3rem;
-width: auto;
-background: magenta;
-transform: scaleX(-1);
+	height: 3rem;
+	width: auto;
+	background: magenta;
+	transform: scaleX(-1);
 }
 
 #previews>:first-child {
-margin-right: 1rem;
+	margin-right: 1rem;
 }
 
-main:hover>#previews>*{
-height: auto;
-width: calc(50% - .5rem);
+main:hover>#previews>* {
+	height: auto;
+	width: calc(50% - .5rem);
 }
 
-#presets,label{
+#presets,
+label {
 	display: flex;
-justify-content: space-between;
-align-items: center
+	justify-content: space-between;
+	align-items: center
 }
 
 #presets:focus-within,
-input:focus{
-border-color: black
+input:focus {
+	border-color: black
 }
 
-#presets>*{
+#presets>* {
 	border: 0;
 	background: transparent;
 	flex-grow: 1;
 }
-#presets>:focus{
-background: black;
-color: white
+
+#presets>:focus {
+	background: black;
+	color: white
 }
 
-#presets>:first-child{
+#presets>:first-child {
 	border-radius: 100px 0 0 100px
 }
 
-#presets>:last-child{
+#presets>:last-child {
 	border-radius: 0 100px 100px 0
 }
 
-label{
-height: 2rem
+label {
+	height: 2rem
 }
 
-label>*{
-width: 80%;
-height: 1.5rem;
-border-radius: 100px;
-border: 0.25rem solid lightgray;
+label>* {
+	width: 80%;
+	height: 1.5rem;
+	border-radius: 100px;
+	border: 0.25rem solid lightgray;
 }
 
 input[type=range] {
--webkit-appearance: none;
---gradient: transparent, transparent;
---rainbow: hsl(0, 80%, 75%), hsl(30, 80%, 75%), hsl(60, 80%, 75%), hsl(90, 80%, 75%), hsl(120, 80%, 75%), hsl(150, 80%, 75%), hsl(180, 80%, 75%), hsl(210, 80%, 75%), hsl(240, 80%, 75%), hsl(270, 80%, 75%), hsl(300, 80%, 75%), hsl(330, 80%, 75%);
-background: linear-gradient(90deg, var(--gradient)), linear-gradient(90deg, var(--rainbow));
+	-webkit-appearance: none;
+	--gradient: transparent, transparent;
+	--rainbow: hsl(0, 80%, 75%), hsl(30, 80%, 75%), hsl(60, 80%, 75%), hsl(90, 80%, 75%), hsl(120, 80%, 75%), hsl(150, 80%, 75%), hsl(180, 80%, 75%), hsl(210, 80%, 75%), hsl(240, 80%, 75%), hsl(270, 80%, 75%), hsl(300, 80%, 75%), hsl(330, 80%, 75%);
+	background: linear-gradient(90deg, var(--gradient)), linear-gradient(90deg, var(--rainbow));
 }
 
 input[type=range]::-webkit-slider-thumb {
--webkit-appearance: none;
-background: white;
-width: 1rem;
-height: 1rem;
-border: 0.25rem solid black;
-border-radius: 0.5rem
+	-webkit-appearance: none;
+	background: white;
+	width: 1rem;
+	height: 1rem;
+	border: 0.25rem solid black;
+	border-radius: 0.5rem
 }
 
 input[type=range]:focus::-webkit-slider-thumb {
-border-color: white;
-background: black;
+	border-color: white;
+	background: black;
 }
 
 input#exposure,
 input#fog,
 input#vignette {
---gradient: black, #8880, white
+	--gradient: black, #8880, white
 }
 
 input#contrast {
---gradient: gray, #8880
+	--gradient: gray, #8880
 }
 
 input#temperature {
---gradient: #88f, #8880, #ff8
+	--gradient: #88f, #8880, #ff8
 }
 
 input#tint {
---gradient: #f8f, #8880, #8f8
+	--gradient: #f8f, #8880, #8f8
 }
 
 input#sepia {
---gradient: #8880, #aa8
+	--gradient: #8880, #aa8
 }
 
 input#hue,
 input#rotate {
-background: linear-gradient(90deg, hsl(0, 80%, 75%), hsl(60, 80%, 75%), hsl(120, 80%, 75%), hsl(180, 80%, 75%), hsl(240, 80%, 75%), hsl(300, 80%, 75%), hsl(0, 80%, 75%), hsl(60, 80%, 75%), hsl(120, 80%, 75%), hsl(180, 80%, 75%), hsl(240, 80%, 75%), hsl(300, 80%, 75%), hsl(0, 80%, 75%))
+	background: linear-gradient(90deg, hsl(0, 80%, 75%), hsl(60, 80%, 75%), hsl(120, 80%, 75%), hsl(180, 80%, 75%), hsl(240, 80%, 75%), hsl(300, 80%, 75%), hsl(0, 80%, 75%), hsl(60, 80%, 75%), hsl(120, 80%, 75%), hsl(180, 80%, 75%), hsl(240, 80%, 75%), hsl(300, 80%, 75%), hsl(0, 80%, 75%))
 }
 
 input#saturate {
---gradient: gray, #8880 50%, blue, magenta
+	--gradient: gray, #8880 50%, blue, magenta
 }
 
 input#blur {
---gradient: #8880, gray
+	--gradient: #8880, gray
 }
 
 input#scale,
 input#x,
 input#y,
 input#pillarbox,
-input#letterbox
-{
---gradient: black, white
+input#letterbox {
+	--gradient: black, white
 }
-
 `
 	form.appendChild(style)
 
