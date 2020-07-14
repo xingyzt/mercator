6e4a7bbacb28fd611aae9dc28cbf6226f5855357
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2020-07-13 18:58:27 -0700

    switch to stylesheet
    also, gradients!

--- mercator-filters/script.js
+++ mercator-filters/script.js
@@ -1,10 +1,16 @@
 async function() {
     'use strict'
 
+    const canvas = document.createElement('canvas')
+
+
     // Create form
 
     const form = document.createElement('form')
-    form.style=`
+
+    const style = document.createElement('style')
+    style.innerText = `
+form{
 position: fixed;
 left: 0;
 top: 0;
@@ -16,7 +22,117 @@ border-radius: 0 0 1vmin 0;
 padding: 1rem;
 transition: opacity 200ms;
 opacity: .2
+}
+
+label{
+display: flex;
+justify-content: space-between
+}
+
+input{
+width: 300px;
+-webkit-appearance: none;
+--gradient: transparent, transparent;
+--rainbow:
+hsl(0,80%,75%),
+hsl(30,80%,75%),
+hsl(60,80%,75%),
+hsl(90,80%,75%),
+hsl(120,80%,75%),
+hsl(150,80%,75%),
+hsl(180,80%,75%),
+hsl(210,80%,75%),
+hsl(240,80%,75%),
+hsl(270,80%,75%),
+hsl(300,80%,75%),
+hsl(330,80%,75%);
+
+background:
+linear-gradient(90deg,var(--gradient)),
+linear-gradient(90deg,var(--rainbow));
+
+border-radius: 100px
+}
+
+input#exposure,
+input#fog,
+input#vignette
+{
+--gradient: black,#8880,white
+}
+
+input#contrast{
+--gradient: gray, #8880
+}
+
+input#temperature{
+--gradient: #4af, #8880, #fa4
+}
+
+input#tint{
+--gradient: #f8f, #8880, #8f8
+}
+
+input#sepia{
+--gradient: #8880, #aa8
+}
+
+input#hue,
+input#rotate{
+
+background: linear-gradient(90deg,hsl(0,80%,75%),
+hsl(30,80%,75%),
+hsl(60,80%,75%),
+hsl(90,80%,75%),
+hsl(120,80%,75%),
+hsl(150,80%,75%),
+hsl(180,80%,75%),
+hsl(210,80%,75%),
+hsl(240,80%,75%),
+hsl(270,80%,75%),
+hsl(300,80%,75%),
+hsl(330,80%,75%),
+hsl(0,80%,75%),
+hsl(30,80%,75%),
+hsl(60,80%,75%),
+hsl(90,80%,75%),
+hsl(120,80%,75%),
+hsl(150,80%,75%),
+hsl(180,80%,75%),
+hsl(210,80%,75%),
+hsl(240,80%,75%),
+hsl(270,80%,75%),
+hsl(300,80%,75%),
+hsl(330,80%,75%)
+)
+}
+
+input#saturate{
+--gradient: gray, #8880 50%, blue, magenta
+}
+
+input#blur{
+--gradient: #8880, gray
+}
+
+input#scale,
+input#x,
+input#y,
+input#pillarbox,
+input#letterbox
+{
+--gradient: black , white
+}
+video{
+height: 50px;
+background: magenta;
+cursor: pointer;
+transform: scaleX(-1)
+}
+
+
 `
+    form.appendChild(style)
 
     form.addEventListener('mouseenter',()=>{
         form.style.opacity = 1
@@ -28,65 +144,43 @@ opacity: .2
 
     // Create sliders
 
-    const sliders = {
+    const sliders = Object.fromEntries(
+        'exposure,contrast,temperature,tint,sepia,hue,saturate,blur,fog,vignette,rotate,scale,x,y,pillarbox,letterbox'
+        .split(',')
+        .map( key => {
 
-        exposure: '',
-        contrast: '',
-        sepia: '',
-        hue: '',
-        saturate: '',
-        blur: '',
-        fog: '',
-        vignette: '',
+            let slider = document.createElement('input')
 
-        rotate: '',
-        scale: '',
-        x: '',
-        y: '',
+            slider.type = 'range'
 
-        pillarbox: '',
-        letterbox: '',
-    }
+            slider.min = [
+                'blur',
+                'sepia',
+                'scale',
+                'pillarbox',
+                'letterbox'
+            ].includes(key) ? 0 : -1
 
-    Object.keys(sliders).forEach(key=>{
-        let slider = document.createElement('input')
-        sliders[key] = slider
-        slider.type = 'range'
+            slider.max = 1
+            slider.step = 0.01
+            slider.value = 0
 
-        slider.min = [
-            'blur',
-            'sepia',
-            'scale',
-            'pillarbox',
-            'letterbox'
-        ].includes(key) ? 0 : -1
+            let label = document.createElement('label')
 
-        slider.max = 1
-        slider.step = 0.01
-        slider.value = 0
-        slider.style = 'width: 300px'
+            label.innerText = slider.title = slider.id = key
 
-        let label = document.createElement('label')
-        label.style = `
-display: flex;
-justify-content: space-between
-`
-        label.innerText = key
-        slider.title = key
+            form.appendChild(label)
+            label.appendChild(slider)
 
-        form.appendChild(label)
-        label.appendChild(slider)
-    })
+            return [key,slider]
+        })
+    )
 
 
     // Create preview video
 
     const video = document.createElement('video')
     video.style=`
-height: 50px;
-background: magenta;
-cursor: pointer;
-transform: scaleX(-1)
 `
     video.setAttribute('playsinline','')
     video.setAttribute('autoplay','')
@@ -99,8 +193,19 @@ transform: scaleX(-1)
         })
     })
 
+    // Create color balance matrix
+
+    const svg = document.createElementNS('http://www.w3.org/2000/svg','svg')
+    const filter = document.createElementNS('http://www.w3.org/2000/svg','filter')
+    filter.id = 'mercator-filters-svg-filter'
+    const filter_matrix = document.createElementNS('http://www.w3.org/2000/svg','feColorMatrix')
+    filter_matrix.setAttribute('in','SourceGraphic')
+
     // Add UI to page
 
+    filter.appendChild(filter_matrix)
+    svg.appendChild(filter)
+    form.appendChild(svg)
     form.appendChild(video)
     document.body.appendChild(form)
 
@@ -110,8 +215,6 @@ transform: scaleX(-1)
             // Copy original stream settings
 
             super(old_stream)
-
-            const canvas = document.createElement('canvas')
 
             const constraints = {audio: false, video: true}
 
@@ -143,16 +246,31 @@ transform: scaleX(-1)
                 sliders.hue.value %= 1
                 sliders.rotate.value %= 1
 
+
+                // BALANCE
+
+                let temperature = sliders.temperature.value*255
+                let tint = sliders.tint.value*255
+                filter_matrix.setAttribute('value',`
+1 0 0 0 0
+0 1 0 0 0
+0 0 2 0 0
+0 0 0 1 0
+`)
+
                 // CSS filters
 
                 canvas_ctx.filter = `
 brightness(${amp**sliders.exposure.value})
 contrast(${amp**sliders.contrast.value})
+url('#mercator-filters-svg-filter')
+url('#mercator-filters-svg-filter')
 sepia(${sliders.sepia.value*100}%)
 hue-rotate(${360*sliders.hue.value}deg)
 saturate(${amp**sliders.saturate.value*100}%)
 blur(${sliders.blur.value*w/32}px)
 `
+
 
                 // Linear transformations: rotation, scaling, translation
 
