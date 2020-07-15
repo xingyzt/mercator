432b83f86c6d9e770dd2de2908d2496e705b09df
Xing <flyorboom@gmail.com>
2020-07-15 13:24:54 -0700

    Update greasyfork version

--- mercator-studio/wrapper.greasyfork.js
+++ mercator-studio/wrapper.greasyfork.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Meet Mini Studio
 // @namespace    http://tampermonkey.net/
-// @version      1.8.1
+// @version      1.9.0
 // @description  Change how you look on Google Meet.
 // @author       Xing
 // @match        https://meet.google.com/*
