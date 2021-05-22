26079cc7ec748d049a418a18a52f3a68fa4102d5
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-05-22 04:43:56 +0000

    bump version to 1.18

--- mercator-studio/script.js
+++ mercator-studio/script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name	Mercator Studio for Google Meet
-// @version	1.17.4
+// @version	1.18.0
 // @description	Change how you look on Google Meet.
 // @author	Xing <dev@x-ing.space> (https://x-ing.space)
 // @copyright	2021, Xing (https://x-ing.space)
