12be1e1e0a79047a5add02b1aee9b2c302a21831
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-05-24 17:10:05 +0000

    gotta separate the moz and webkit selectors

--- mercator-studio/script.js
+++ mercator-studio/script.js
@@ -292,6 +292,13 @@ input[type=range]::-webkit-slider-thumb {
 	transform: scale(1.5);
 	border-radius: 100%;
 }
+input[type=range]:hover::-webkit-slider-thumb {
+	background: var(--bg-hov);
+}
+input[type=range]:focus::-webkit-slider-thumb {
+	border-color: var(--bg);
+	background: var(--txt);
+}
 input[type=range]::-moz-range-thumb {
 	background: var(--bg);
 	width: 1rem;
@@ -301,11 +308,9 @@ input[type=range]::-moz-range-thumb {
 	border-radius: 100%;
 	box-sizing: border-box;
 }
-input[type=range]:hover::-webkit-slider-thumb,
 input[type=range]:hover::-moz-range-thumb {
 	background: var(--bg-hov);
 }
-input[type=range]:focus::-webkit-slider-thumb,
 input[type=range]:focus::-moz-range-thumb {
 	border-color: var(--bg);
 	background: var(--txt);
